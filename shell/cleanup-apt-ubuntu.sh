89d0467eed26921ed80f7b287cbc72aac3b11e54
givanse <silvag777@gmail.com>
2013-12-15 14:07:16 -0600

    prompt for confirmation and list held packages

--- shell/cleanup-apt-ubuntu.sh
+++ shell/cleanup-apt-ubuntu.sh
@@ -3,12 +3,18 @@
 # Fix and cleanup an Aptitude system
 
 # Requirements: Aptitude, deborphan
-
-# Warning: This script will remove old kernels.
-#          If you have recently installed new kernels, run after a reboot.
+# sudo apt-get install deborphan
 
 set -e
 
+echo 'Warning: This script will remove old kernels without prompt.
+         If you have recently installed new kernels, run after a reboot.'
+read -e -p 'Do you want to continue? [yes|no]: ' -i 'no' response
+if [[ $response != 'yes' ]] 
+then
+    echo 'Nothing will be done.'
+    exit
+fi
 
 sudo dpkg --configure -a
 
@@ -23,15 +29,19 @@ dpkg -l 'linux-*' | sed -e "$sed_expr" | xargs sudo apt-get -y remove --purge
 # update grub
 sudo update-grub2
 
-echo "> Auto remove clean"
+echo "> Auto remove and clean"
 sudo apt-get autoremove
 sudo apt-get autoclean
+sudo apt-get clean
 
-echo "> Orphaned"
+echo "> Remove orphaned"
 # remove orphaned packages
 sudo deborphan | xargs sudo apt-get -y remove --purge
 # remove orphaned data packages
 sudo deborphan --guess-data | xargs sudo apt-get -y remove --purge
+
+echo "> List held packages"
+dpkg --get-selections | grep hold
 
 exit
 #EOF
