b119824f1d55b0b6c13b048abd917fc24b3cb142
givanse <silvag777@gmail.com>
2014-01-07 16:17:47 -0600

    fix conf files path and call customize-apt-packages.sh

--- shell/customize-lubuntu.sh
+++ shell/customize-lubuntu.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+    #!/bin/bash
 
 set -e
+
+./customize-apt-packages.sh
 
 #sudo sed -i 's/quiet splash/text/' /etc/default/grub
 #sudo sed -i 's/#GRUB_TERMINAL/GRUB_TERMINAL/' /etc/default/grub
@@ -11,11 +13,11 @@ rm -vr Documents Music Pictures Public Templates Videos
 cd -
 
 # Add config files
-cp -v '.vimrc' ~/.vimrc
-cp -v  'lxterminal.conf' ~/.config/lxterminal/lxterminal.conf
+cp -v ./conf/.vimrc ~/.vimrc
+cp -v  ./conf/lxterminal.conf ~/.config/lxterminal/lxterminal.conf
 
-terminal='x-terminal-emulator --geometry=90x24'
-sed -i "s/x-terminal-emulator/$terminal/" ~/.config/openbox/lubuntu-rc.xml 
+x_terminal_emulator='x-terminal-emulator --geometry=90x24'
+sed -i "s/x-terminal-emulator/$x_terminal_emulator/" ~/.config/openbox/lubuntu-rc.xml 
 openbox --reconfigure
 
 #EOF
