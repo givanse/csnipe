d7262ea3fe425e62c4810800d3a55b2f0b4b690a
givanse <silvag777@gmail.com>
2014-01-08 08:30:24 -0600

    remove xterm

--- shell/customize-apt-packages.sh
+++ shell/customize-apt-packages.sh
@@ -10,7 +10,8 @@ games='ace-of-penguins gnome-game*'
 office='libreoffice* abiword* gnumeric* simple-scan xpad'
 media='audacious* gnome-mplayer xfburn guvcview'
 other='compiz* blueman update-manager lubuntu-software-center'
-sudo apt-get --purge remove $web $games $office $media $other 
+system_tools='xterm'
+sudo apt-get --purge remove $web $games $office $media $other $system_tools 
 
 # INSTALL
 apps='ubuntuone-control-panel-qt vim'
